f79de822105fb0b40bc46c2afc0bfd08cdc44dea
Abhinav Joshi <mag@Mags-Mac.local>
2018-10-01 20:38:12 -0500

    5th program

---
+++ python5.py
@@ -0,0 +1,13 @@
+myName = "Abhinav Joshi"
+myAge = 35
+myHeight = 175
+myWeight = 150
+myEyes = "Brown"
+myTeeth = "White"
+myHair = "Brown"
+
+print "Let's talk about me, my name is",myName
+print "My age is",myAge, "years."
+print "My height is",myHeight,"cm."
+print "My weight is",myWeight,"lbs."
+print "Color of my eyes is",myEyes,"."
